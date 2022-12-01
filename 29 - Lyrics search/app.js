fb1406345ff5c9c3b2c20111e911a5022aace772
Sachin Yadav <yadavsachin85@gmail.com>
2022-12-01 21:02:30 +0530

    Add files via upload

---
+++ 29 - Lyrics search/app.js
@@ -0,0 +1,83 @@
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
+
+const url = "https://api.lyrics.ovh";
+
+async function searchSong(term) {
+  const res = await fetch(`${url}/suggest/${term}`);
+  const data = await await res.json();
+  showData(data);
+}
+
+function showData(data) {
+  result.innerHTML = `<ul class='songs'>
+                        ${data.data
+                          .map(
+                            (song) => `<li>
+                                        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                                        <button class='btn' data-artist='${song.artist.name}' data.songtitle='${song.title}'>Get Lyrics</button>
+                                      </li>`
+                          )
+                          .join(" ")}
+                        </ul>`;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+                              ${
+                                data.prev
+                                  ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+                                  : " "
+                              }
+                              ${
+                                data.next
+                                  ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+                                  : " "
+                              }
+                            `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+async function getMoreSongs(url) {
+  const res = await fetch(url);
+  const data = await await res.json();
+
+  showData(data);
+}
+
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${url}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+
+  more.innerHTML = "";
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+  if (!searchTerm) {
+    alert("Please Input the song");
+  } else {
+    searchSong(searchTerm);
+  }
+});
+
+result.addEventListener("click", (e) => {
+  const clicked = e.target;
+
+  if (clicked.classList.contains("btn")) {
+    const artist = clicked.getAttribute("data-artist");
+    const songTitle = clicked.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
+  }
+});
